9d8d55d17ad9cf8e05214e03afe4e6f4dfb3b403
shohidul2015 <shd_islam@hotmail.com>
2025-09-28 17:13:49 +0200

    Add: ABout section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
-import Hero from './Hero'
-import Navbar from './Navbar'
+import CompanyLogo from './components/CompanyLogo'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
+    <Navbar />
     <Hero />
+    <CompanyLogo />
     </>
   )
 }
