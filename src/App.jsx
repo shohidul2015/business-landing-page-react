a97451fc8b01f6eb421128d06467b4ed0ef2340d
shohidul2015 <shd_islam@hotmail.com>
2025-09-28 19:39:59 +0200

    Add: PurposeSection component added to shoe purposes...

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
-import CompanyLogo from './components/CompanyLogo'
-import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import CompanyLogo from './components/CompanyLogo'
+import PurposeSection from './components/PurposeSection'
 
 function App() {
-  
-
   return (
-    <>
-    <Navbar />
-    <Hero />
-    <CompanyLogo />
-    </>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+        <CompanyLogo />
+        <PurposeSection />
+  
+      </div>
+    </main>
   )
 }
 
